d9fe04d00391b0ec3392c87e553a32747032ed87
matteo-didone <matteo.dido9@gmail.com>
2023-06-15 18:04:20 +0200

    Working on the basic header structure

--- vite-comics/src/components/AppHeader.vue
+++ vite-comics/src/components/AppHeader.vue
@@ -1,6 +1,16 @@
 <template>
     <header>
-        
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="dc-comics-logo">
+        </div>
+
+        <nav class="navbar">
+            <ul>
+                <li>
+
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
@@ -19,6 +29,21 @@
 
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+// Scoped styles
+    @use '/Users/matteodidone/VS Code Projects/vite-comics/vite-comics/src/styles/partials/_mixins.scss' as *; 
+    @use '/Users/matteodidone/VS Code Projects/vite-comics/vite-comics/src/styles/partials/_variables.scss' as *;
+
+    header{
+        display: flex;
+
+        div.logo img{
+            width: 100%;
+        }
+
+        nav.navbar ul{
+            @include flex();
+        }
+    }
 
 </style>
