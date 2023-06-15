9a6b07447074801feffd7ca535f58298022b38b1
matteo-didone <matteo.dido9@gmail.com>
2023-06-15 18:43:32 +0200

    Creating basic footer structure

--- vite-comics/src/components/AppFooter.vue
+++ vite-comics/src/components/AppFooter.vue
@@ -1,5 +1,48 @@
 <template>
     <footer>
+
+        <div class="footer-left">
+            <button class="footer-button">SIGN-UP NOW!</button>
+        </div>
+
+        <div class="footer-right">
+
+            <h3> FOLLOW US </h3>
+
+            <ul>
+                <li>
+                    <div class="social-link">
+                        <img src="/Users/matteodidone/VS Code Projects/vite-comics/vite-comics/src/assets/img/footer-facebook.png" alt="Facebook Logo">
+                    </div>
+                </li>
+
+                <li>
+                    <div class="social-link">
+                        <img src="/Users/matteodidone/VS Code Projects/vite-comics/vite-comics/src/assets/img/footer-twitter.png" alt="Twitter Logo">
+                    </div>
+                </li>
+
+                <li>
+                    <div class="social-link">
+                        <img src="/Users/matteodidone/VS Code Projects/vite-comics/vite-comics/src/assets/img/footer-youtube.png" alt="YouTube Logo">
+                    </div>
+                </li>
+
+                <li>
+                    <div class="social-link">
+                        <img src="/Users/matteodidone/VS Code Projects/vite-comics/vite-comics/src/assets/img/footer-pinterest.png" alt="Pinterest Logo">
+                    </div>
+                </li>
+
+                <li>
+                    <div class="social-link">
+                        <img src="/Users/matteodidone/VS Code Projects/vite-comics/vite-comics/src/assets/img/footer-periscope.png" alt="Periscope Logo">
+                    </div>
+                </li>
+
+            </ul>
+        </div>
+
     </footer>
 </template>
 
@@ -19,5 +62,10 @@
 </script>
 
 <style lang="scss">
+
+    footer{
+        height: 50px;
+        background-color: #303030;
+    }
 
 </style>
