dd648b0a2f42925aef820a705d6aae70b1b0eafc
matteo-didone <matteo.dido9@gmail.com>
2023-06-15 19:26:45 +0200

    Populating footer top left

--- vite-comics/src/components/AppFooter.vue
+++ vite-comics/src/components/AppFooter.vue
@@ -8,27 +8,144 @@
                 <div class="first-column">
 
                     <div class="dc-comics">
+                        <h2>DC COMICS</h2>
 
+                        <ul>
+
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+
+                            <li>
+                                <a href="#">Comics</a>
+                            </li>
+
+                            <li>
+                                <a href="#">Movies</a>
+                            </li>
+
+                            <li>
+                                <a href="#">TV</a>
+                            </li>
+
+                            <li>
+                                <a href="#">Games</a>
+                            </li>
+
+                            <li>
+                                <a href="#">Videos</a>
+                            </li>
+
+                            <li>
+                                <a href="#">News</a>
+                            </li>
+
+                        </ul>
                     </div>
 
                     <div class="shop">
+                        <h2>SHOP</h2>
 
+                        <ul>
+
+                            <li>
+                                <a href="#">Shop DC</a>
+                            </li>
+
+                            <li>
+                                <a href="#">Shop DC Collectibles</a>
+                            </li>
+
+                        </ul>
                     </div>
 
                 </div>
 
-                <div class="second-column">
+                <div class="second-column dc">
+                    <h2>DC</h2>
 
+                    <ul>
+
+                        <li>
+                            <a href="#">Terms Of Use</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Privacy Policy (New)</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Ad Choices</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Advertising</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Jobs</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Subscriptions</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Talent Workshops</a>
+                        </li>
+
+                        <li>
+                            <a href="#">CPSC Certificates</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Ratings</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Shop Help</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Contact Us</a>
+                        </li>
+
+                    </ul>
                 </div>
 
-                <div class="third-column">
+                <div class="third-column sites">
+                    <h2>SITES</h2>
 
+                    <ul>
+
+                        <li>
+                            <a href="#">DC</a>
+                        </li>
+
+                        <li>
+                            <a href="#">MAD Magazine</a>
+                        </li>
+
+                        <li>
+                            <a href="#">DC Kids</a>
+                        </li>
+
+                        <li>
+                            <a href="#">DC Universe</a>
+                        </li>
+
+                        <li>
+                            <a href="#">DC Power Visa</a>
+                        </li>
+
+                    </ul>
+                    
                 </div>
 
             </div>
 
             <div class="footer-top-right">
-                
+
             </div>
 
         </div>
@@ -120,4 +237,5 @@ footer {
         height: 100px;
     }
 
-}</style>
+}
+</style>
